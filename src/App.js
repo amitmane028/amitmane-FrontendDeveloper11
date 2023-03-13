19b28f9dc28bbe243b0b449a151028b8a18d0ce8
amit <amit.mane028@gmail.com>
2023-03-13 12:07:16 +0530

    tgline commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div>
         <Header />
         <Banner />
+      
       </div>
     </>
   );
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React from "react";
 
 function Banner() {
   return (
-    <div>
-      <img src="./image/rocket2.jpg" alt="rocketImage" className="image1" />
-    </div>
-  )
+    <>
+      <div className="banner">
+        <img src="./image/rocket2.jpg" alt="rocketImage"  />
+       <div className="tagline">
+      I started SpaceX with 
+        expectation of failure...
+
+
+        -ELON MUSK.
+     
+       </div>
+    
+      </div>
+    </>
+  );
 }
-  
-export default Banner
+
+export default Banner;
