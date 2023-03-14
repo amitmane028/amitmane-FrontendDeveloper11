34ed2d5904ade71f88d4b7199bd3d3579e8b8ebc
amit <amit.mane028@gmail.com>
2023-03-14 09:48:31 +0530

    setup pagination

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -1,18 +1,43 @@
 import React, { useState, useEffect } from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import { Pagination } from "react-bootstrap";
 
 function ApiData() {
     const [data, setData] = useState([]);
     const columns = [
         { dataField: 'id', text: 'id' },
-        { dataField: 'type', text: 'type' },
-        { dataField: 'serial', text: 'serial' },
-        { dataField: 'status', text: 'status' },
-        { dataField: 'reuse_count', text: 'reuse_count' },
-        { dataField: 'water_landings', text: 'water_landings' }
+        { dataField: 'type', text: 'type' ,sort:true},
+        { dataField: 'serial', text: 'serial',sort:true },
+        { dataField: 'status', text: 'status',sort:true },
+        { dataField: 'reuse_count', text: 'reuse_count',sort:true },
+        { dataField: 'water_landings', text: 'water_landings',sort:true }
     
     ]
+
+    const pagination=paginationFactory({
+      page:1,
+      sizePerPage:5,
+      lastPageText:'>>',
+      firstPageText:'<<',
+      nextPageText:'>',
+      prePageText:'<',
+      showTotal:true,
+      alwaysShowAllBtns:true,
+      onPageChange:function(page,sizePerPage){
+        console.log('page',page);
+        console.log('sizePerPage',sizePerPage);
+      },
+      onSizePerPageChange:function(page,sizePerPage){
+        console.log('page',page);
+        console.log('sizePerPage',sizePerPage);
+      }
+
+    });
+
     useEffect(() => {
         fetch("https://api.spacexdata.com/v4/capsules")
             .then((response) => response.json())
@@ -22,7 +47,11 @@ function ApiData() {
     return (
         <>
             <div>
-                <BootstrapTable keyField="id" columns={columns} data={data} />
+                <BootstrapTable bootstrap4 keyField="id"
+                 columns={columns} 
+                 data={data} 
+                 pagination={pagination}
+                 />
                 {/* <table>
                     <tr>
                         <th>id</th>
